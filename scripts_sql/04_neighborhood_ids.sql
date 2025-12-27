76740edaa4dc0c5965705f1d249c4e430f1fdcac
Oscar Cruz <cruzd.oscar@gmail.com>
2025-12-27 16:07:14 -0500

    Se añade script SQL para obtener los IDs de los vecindarios O'Hare y Loop.

---
+++ scripts_sql/04_neighborhood_ids.sql
@@ -0,0 +1,8 @@
+-- Esta consulta encuentra los IDs de vecindarios O'Hare y Loop de la tabla neighborhoods.
+SELECT 
+    name,
+    neighborhood_id
+FROM 
+    neighborhoods
+WHERE 
+    name in ('O''Hare', 'Loop');
