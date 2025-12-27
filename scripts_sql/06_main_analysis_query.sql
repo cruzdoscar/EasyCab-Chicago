d966ce488b579a203908f844b9967caed847d224
Oscar Cruz <cruzd.oscar@gmail.com>
2025-12-27 16:44:24 -0500

    Se añade comentario explicativo sobre el objetivo de la consulta SQL para vincular duración de viajes con clima.

--- scripts_sql/06_main_analysis_query.sql
+++ scripts_sql/06_main_analysis_query.sql
@@ -1,4 +1,5 @@
 -- Esta solicitud obtiene los viajes que comenzaron en Loop y terminaron en O'Hare un sábado, junto con las condiciones climáticas y la duración del viaje.
+-- Objetivo: Vincular duración de viajes con clima para prueba de hipótesis 
 
 
 SELECT
@@ -13,6 +14,8 @@ FROM
 INNER JOIN
     weather_records AS w ON w.ts = t.start_ts
 WHERE
-    (t.pickup_location_id = 50 AND t.dropoff_location_id = 63) AND EXTRACT(DOW FROM t.start_ts) = 6 -- Filtramos por viajes que comenzaron en Loop (ID 50) y terminaron en O'Hare (ID 63) un sábado (DOW 6)
+    t.pickup_location_id = 50 
+    AND t.dropoff_location_id = 63
+    AND EXTRACT(DOW FROM t.start_ts) = 6 -- Filtramos por viajes que comenzaron en Loop (ID 50) y terminaron en O'Hare (ID 63) un sábado (DOW 6)
 ORDER BY
     t.trip_id;
